4e827646f4f2b776f1acb58f9f59c889ffbcc2ba
bigflopz <chris.stappas@gmail.com>
2024-09-09 19:04:49 -0400

    fixed publish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import CardList from './components/CardList';
